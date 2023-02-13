1c7b9920f8c950249c322b0aabca52008d8f8fcf
Paolo224 <picariellopaolo224@gmail.com>
2023-02-13 17:18:38 +0100

    Create migrations seeder

---
+++ database/migrations/2023_02_13_155533_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->float('price', 6, 2);
+            $table->string('series', 120);
+            $table->date('sale_date', 120);
+            $table->string('type', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ComicController as ComicController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [ComicController::class, 'index'])->name('home');
