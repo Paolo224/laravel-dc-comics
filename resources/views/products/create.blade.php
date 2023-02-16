61df80a3f072570d2d0e153f946a8c2f4f53e3a2
Paolo224 <picariellopaolo224@gmail.com>
2023-02-16 16:23:04 +0100

    old

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -28,43 +28,43 @@
                         <label class="form-label">
                             Titolo
                         </label>
-                        <input type="text" class="form-control" name="title">
+                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Descrizione
                         </label>
-                        <textarea class="form-control" name="description" cols="30" rows="10"></textarea>
+                        <textarea class="form-control" name="description" cols="30" rows="10">{{ old('description') }}</textarea>
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Immagine
                         </label>
-                        <input type="text" class="form-control" name="thumb">
+                        <input type="text" class="form-control" name="thumb" value="{{ old('thumb') }}">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Prezzo
                         </label>
-                        <input type="text" class="form-control" name="price">
+                        <input type="text" class="form-control" name="price" value="{{ old('price') }}">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Serie
                         </label>
-                        <input type="text" class="form-control" name="series">
+                        <input type="text" class="form-control" name="series" value="{{ old('series') }}">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Data di vendita
                         </label>
-                        <input type="date" class="form-control" name="sale_date">
+                        <input type="date" class="form-control" name="sale_date" value="{{ old('sale_date') }}">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">
                             Tipo
                         </label>
-                        <input type="text" class="form-control" name="type">
+                        <input type="text" class="form-control" name="type" value="{{ old('type') }}">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
